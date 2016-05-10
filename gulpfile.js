a88b177946cac87c7905dd2eebd95b88e6a92f2a
Mark Buchthal <mark.buchthal@gmail.com>
2016-05-10 11:30:42 -0700

    add angular functionality and css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 const gulp = require('gulp');
+const eslint = require('gulp-eslint');
 const webpack = require('webpack-stream');
+
+var files = ['server.js', 'gulpfile.js', 'app/**/*.js'];
 
 gulp.task('webpack:dev', () => {
   gulp.src('app/js/entry.js')
@@ -8,12 +11,18 @@ gulp.task('webpack:dev', () => {
         filename: 'bundle.js'
       }
     }))
-    .pipe(gulp.dest('./build'))
+    .pipe(gulp.dest('./build'));
 });
 
 gulp.task('static:dev', () => {
   gulp.src(['./app/**/*.html', './app/**/*.css'])
     .pipe(gulp.dest('./build'));
+});
+
+gulp.task('lint', () => {
+  return gulp.src(files)
+    .pipe(eslint())
+    .pipe(eslint.format());
 });
 
 gulp.task('build:dev', ['webpack:dev', 'static:dev']);
